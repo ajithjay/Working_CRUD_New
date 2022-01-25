0a05d73b072e9d8b6343ca66fff08d3637575dc9
Ajith <ajith.a@selloplus.com>
2022-01-25 12:34:32 +0530

    Angular CRUD

---
+++ src/app/student-dashboard/student-dashboard.component.ts
@@ -0,0 +1,124 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { Student } from '../student.model';
+
+
+@Component({
+  selector: 'app-student-dashboard',
+  templateUrl: './student-dashboard.component.html',
+  styleUrls: ['./student-dashboard.component.css']
+})
+export class StudentDashboardComponent implements OnInit {
+
+  studentValue!:FormGroup;
+  studentObj: Student = new Student ;
+  studentList : any = [];
+  btnSaveShow:boolean = true;
+  btnUpdateShow:boolean = false;
+  
+
+  constructor(private fb:FormBuilder, private api:ApiService) { }
+
+  ngOnInit(): void {
+    this.studentValue=this.fb.group({
+      name:[''],
+      class:[''],
+      email:[''],
+      mobile:['']
+    })
+    this.getStudent();
+  }
+ AddStudent(){
+   this.studentObj.name = this.studentValue.value.name;
+   this.studentObj.class = this.studentValue.value.class;
+   this.studentObj.email = this.studentValue.value.email;
+   this.studentObj.mobile = this.studentValue.value.mobile;
+
+   this.api.postStudent(this.studentObj).subscribe({next:(v)=>{
+     console.log(v)
+   },
+   error:(e)=>{
+     console.log(e)
+     alert("Error")
+   },
+   complete:()=>{
+     console.log('complete')
+     alert("student record added!")
+     this.getStudent();
+     this.studentValue.reset();
+     
+   }
+  
+  })
+
+ }
+ getStudent(){
+  this.api.getStudent().subscribe(res=>{
+    this.studentList = res;
+  })
+ }
+deleteStudent(data:any){
+  this.api.deleteStudent(data.id).subscribe({next:(v)=>{
+    console.log(v)
+  },
+  error:(e)=>{
+    console.log(e)
+    alert("Error")
+  },
+  complete:()=>{
+    console.log('complete')
+    alert("student record deleted!")
+    this.getStudent();
+  
+  }
+ 
+ })
+
+}
+
+editStudent(data:any){
+this.studentValue.controls["name"].setValue(data.name);
+this.studentValue.controls["class"].setValue(data.class);
+this.studentValue.controls["email"].setValue(data.email);
+this.studentValue.controls["mobile"].setValue(data.mobile);
+this.studentObj.id = data.id;
+this.showUpdate();
+
+}
+updateStudent(){
+  this.studentObj.name = this.studentValue.value.name;
+  this.studentObj.class = this.studentValue.value.class;
+  this.studentObj.email = this.studentValue.value.email;
+  this.studentObj.mobile = this.studentValue.value.mobile;
+  
+  this.api.putStudent(this.studentObj, this.studentObj.id).subscribe({next:(v)=>{
+    console.log(v)
+  },
+  error:(e)=>{
+    console.log(e)
+    alert("Error")
+  },
+  complete:()=>{
+    console.log('student record updated!')
+    alert("student record updated!")
+    this.getStudent();
+    this.studentValue.reset();
+    this.showSave();
+    this.studentObj.id = 0;
+  }
+ 
+ })
+
+}
+  showSave(){
+    this.btnSaveShow = true;
+    this.btnUpdateShow = false;
+  }
+
+  showUpdate(){
+    this.btnSaveShow = false;
+    this.btnUpdateShow = true;
+  }
+}
+
